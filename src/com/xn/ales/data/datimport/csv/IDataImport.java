9865cff3ca0a10310fb8149c58edd21159c9715f
ewanggu <guan.g.wang@ericsson.com>
2016-02-25 16:12:16 +0800

    update

--- src/com/xn/ales/data/datimport/csv/IDataImport.java
+++ src/com/xn/ales/data/datimport/csv/IDataImport.java
@@ -1,17 +1,7 @@
-/**
- * -----------------------------------------------------------------------
- *     Copyright (C) 2016 LM Ericsson Limited.  All rights reserved.
- * -----------------------------------------------------------------------
- */
 package com.xn.ales.data.datimport.csv;
 
 import java.util.List;
 
-/**
- * @author ewanggu
- * @since 2016
- *
- */
 public interface IDataImport {
 
     public void parse(String file);
